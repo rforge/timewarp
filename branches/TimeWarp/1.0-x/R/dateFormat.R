f68187a9dba94adf239ec0de4c916f1c3b5647d2
tplate <tplate@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-09 03:07:36 +0000

    branch to make dateSeq(), dateWarp() etc generics to work with char, Date & POSIXct (so that they return the same type of object they are passed)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/timewarp@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/TimeWarp/1.0-x/R/dateFormat.R
@@ -0,0 +1,34 @@
+##
+## Unfortunately, 'format.POSIXct' is not compatible between
+## Linux and Windows R. Here are a few for the difference I have found:
+## 1) Windows version does not recognize "%y" format.
+## 2) Windows version does not recognize width arguments like
+##    "%02d".
+## 3) Windows and Linux does not agree on the meaning of "%Y".
+##    Under Windows it means "%04Y"; under Linux it print with
+##    minimal width.
+##
+## This function formats any kind of data objects to character
+## strings with the default format "%02m-%02d-%04Y" under
+## both Linux and Windows.
+##
+dateFormat <- function(date, format = NULL)
+{
+    if (is.null(format))
+    {
+        if (Sys.info()[["sysname"]] == "Linux")
+            format <- "%04Y-%02m-%02d"
+        else
+            format <- "%Y-%m-%d"
+    }
+
+    if (is(date, "character"))
+        date <- dateParse(date)
+
+    if (is(date, "dates"))
+        format(as.POSIXct(date), format)
+    else if (is(date, "Date") || is(date, "POSIXt"))
+        format(date, format)
+    else
+        stop("unknow date format: '", class(date), "'")
+}
