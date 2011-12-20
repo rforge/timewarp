a4e6f0b1a50ef21e2bfa57cd963b088a77703c5d
tplate <tplate@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-20 02:36:39 +0000

    Figure out thru trial and error whether the system can handle %04Y formats etc, and whether it needs them. This is intended to get around the failures on Solaris, which I don't have access to.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/timewarp@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TimeWarp/R/dateFormat.R
+++ pkg/TimeWarp/R/dateFormat.R
@@ -5,27 +5,53 @@
 ## 2) Windows version does not recognize width arguments like
 ##    "%02d".
 ## 3) Windows and Linux does not agree on the meaning of "%Y".
-##    Under Windows it means "%04Y"; under Linux it print with
+##    Under Windows it means "%04Y"; under Linux it prints with
 ##    minimal width.
 ##
 ## This function formats any kind of data objects to character
 ## strings with the default format "%02m-%02d-%04Y" under
 ## both Linux and Windows.
 ##
+
+dateFormat.env <- new.env()
+dateFormat.env$initialized <- FALSE
+
+dateFormat.config <- function() {
+    # figure out the capabilities of the date formatting on this system
+    # Use the year 0045 as a test case.  Some systems that accept a %04Y
+    # format will format %Y 1991 as '1991', but %Y 0045 as '45'.
+    if (strftime(as.POSIXct('0045-01-02'), '%Y-%m-%d') == '0045-01-02')
+        dateFormat.env$default.format <- '%Y-%m-%d'
+    else if (strftime(as.POSIXct('0045-01-02'), '%04Y-%m-%d') == '0045-01-02')
+        dateFormat.env$default.format <- '%04Y-%m-%d'
+    else if (strftime(as.POSIXct('0045-01-02'), '%04Y-%02m-%02d') == '0045-01-02')
+        dateFormat.env$default.format <- '%04Y-%02m-%02d'
+    else if (strftime(as.POSIXct('0045-01-02'), '%Y-%02m-%02d') == '0045-01-02')
+        dateFormat.env$default.format <- '%Y-%02m-%02d'
+    else
+        dateFormat.env$default.format <- '%Y-%m-%d'
+
+    if (strftime(as.POSIXct('0045-01-02'), '%04Y')=='0045')
+        dateFormat.env$f04Y.ok <- TRUE
+    else
+        dateFormat.env$f04Y.ok <- FALSE
+
+    if (strftime(as.POSIXct('1991-01-02'), '%02m-%02d')=='01-02')
+        dateFormat.env$f02md.ok <- TRUE
+    else
+        dateFormat.env$f02md.ok <- FALSE
+}
+
 dateFormat <- function(date, format = NULL)
 {
+    if (!dateFormat.env$initialized)
+        dateFormat.config()
     if (is.null(format))
-    {
-        if (Sys.info()[["sysname"]] == "Linux")
-            format <- "%04Y-%02m-%02d"
-        else
-            format <- "%Y-%m-%d"
-    }
-    if (Sys.info()[['sysname']] == 'Windows') {
-        format <- gsub('%04Y', '%Y', format)
-        format <- gsub('%02m', '%m', format)
-        format <- gsub('%02d', '%d', format)
-    }
+        format <- dateFormat.env$default.format
+    if (!dateFormat.env$f04Y.ok && regexpr('%04Y', format, fixed=TRUE)>0)
+        format <- gsub('%04Y', '%Y', format, fixed=TRUE)
+    if (!dateFormat.env$f02md.ok && regexpr('%02', format, fixed=TRUE)>0)
+        format <- gsub('%02', '%', format, fixed=TRUE)
 
     if (is.character(date))
         date <- dateParse(date)
