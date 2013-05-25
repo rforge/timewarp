ecf506bb5c2b55dc21ee85f4b554e83dad9f58c6
tplate <tplate@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-25 01:00:09 +0000

    untabify & fix indenting
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/timewarp@42 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TimeWarp/R/dateMatch.R
+++ pkg/TimeWarp/R/dateMatch.R
@@ -77,124 +77,124 @@ dateMatch.Date <- function(x, table, how=c("NA", "before", "after", "nearest", "
         if (any(diff(table) <= 0))
             stop("table must contain strictly increasing dates")
 
-		# Date vectors can use match() for exact matches with NA's for no match
+        # Date vectors can use match() for exact matches with NA's for no match
         idx <- match(x,table)
-		idxNA <- is.na(idx)
-		N <- length(table)
+        idxNA <- is.na(idx)
+        N <- length(table)
 
-		if (how=="NA"){
-			if (any(idxNA)){
-				if (error.how=='NA' || error.how=='nearest') {
-					idx[idxNA] <- nomatch
-				} else if (error.how=='drop'){
-					idx <- idx[!idxNA]
-				} else if (error.how=='stop'){
-                                    stop('no match found for ', sum(idxNA), ' dates, e.g.: ',
-                                         paste(x[which(idxNA)[seq(1,min(3,sum(idxNA)))]], collapse=', '))
-				}
-			}
-		} else if (how=="before"){
-			if (any(idxNA)){
-				# findInterval() returns the left side of the interval (i.e the "before" index),
-				# 0 if x[i] <- min(table),
-				# and N if x[i] > table[N]
-				idx[idxNA] <- findInterval(x[idxNA],table)
+        if (how=="NA"){
+            if (any(idxNA)){
+                if (error.how=='NA' || error.how=='nearest') {
+                    idx[idxNA] <- nomatch
+                } else if (error.how=='drop'){
+                    idx <- idx[!idxNA]
+                } else if (error.how=='stop'){
+                    stop('no match found for ', sum(idxNA), ' dates, e.g.: ',
+                         paste(x[which(idxNA)[seq(1,min(3,sum(idxNA)))]], collapse=', '))
+                }
+            }
+        } else if (how=="before"){
+            if (any(idxNA)){
+                # findInterval() returns the left side of the interval (i.e the "before" index),
+                # 0 if x[i] <- min(table),
+                # and N if x[i] > table[N]
+                idx[idxNA] <- findInterval(x[idxNA],table)
 
-				if (error.how=='NA') {
-					idx[idx==0] <- nomatch
-				} else if (error.how=='drop'){
-					idx <- idx[idx>0]
-				} else if (error.how=='nearest') {
-					idx[idx==0] <- 1
-				} else if (error.how=='stop'){
-                                    idxBad <- idx==0
-                                    if (any(idxBad))
-                                        stop('no match found for ', sum(idxBad), ' dates, e.g.: ',
-                                             paste(x[which(idxBad)[seq(1,min(3,sum(idxBad)))]], collapse=', '))
-				}
-			}
-		} else if (how=="after"){
-			if (any(idxNA)){
-				idx[idxNA] <- findInterval(x[idxNA],table) + 1
+                if (error.how=='NA') {
+                    idx[idx==0] <- nomatch
+                } else if (error.how=='drop'){
+                    idx <- idx[idx>0]
+                } else if (error.how=='nearest') {
+                    idx[idx==0] <- 1
+                } else if (error.how=='stop'){
+                    idxBad <- idx==0
+                    if (any(idxBad))
+                        stop('no match found for ', sum(idxBad), ' dates, e.g.: ',
+                             paste(x[which(idxBad)[seq(1,min(3,sum(idxBad)))]], collapse=', '))
+                }
+            }
+        } else if (how=="after"){
+            if (any(idxNA)){
+                idx[idxNA] <- findInterval(x[idxNA],table) + 1
 
-				if (error.how=='NA') {
-					idx[idx>N] <- nomatch
-				} else if (error.how=='drop'){
-					idx <- idx[idx<=N]
-				} else if (error.how=='nearest') {
-					idx[idx>N] <- N
-				} else if (error.how=='stop'){
-                                    idxBad <- idx>N
-                                    if (any(idxBad))
-                                        stop('no match found for ', sum(idxBad), ' dates, e.g.: ',
-                                             paste(x[which(idxBad)[seq(1,min(3,sum(idxBad)))]], collapse=', '))
-				}
-			}
-		} else if (how=="nearest"){
-			if (any(idxNA)){
-				x_idxNA <- x[idxNA]
-				y <- findInterval(x_idxNA,table)
+                if (error.how=='NA') {
+                    idx[idx>N] <- nomatch
+                } else if (error.how=='drop'){
+                    idx <- idx[idx<=N]
+                } else if (error.how=='nearest') {
+                    idx[idx>N] <- N
+                } else if (error.how=='stop'){
+                    idxBad <- idx>N
+                    if (any(idxBad))
+                        stop('no match found for ', sum(idxBad), ' dates, e.g.: ',
+                             paste(x[which(idxBad)[seq(1,min(3,sum(idxBad)))]], collapse=', '))
+                }
+            }
+        } else if (how=="nearest"){
+            if (any(idxNA)){
+                x_idxNA <- x[idxNA]
+                y <- findInterval(x_idxNA,table)
 
-				# Not efficient! But what's the alternative look like?
-				for (i in 1:length(y)){
-					if (y[i] == 0){
-						y[i] <- 1
-					} else if (y[i] == N){
-						d2 <- table[N] - x_idxNA[i]
-						d1 <- x_idxNA[i] - table[N-1]
-						if (d2 < d1) y[i] <- N
-						else y[i] <- N-1
-					} else {
-						d2 <- table[y[i]+1] - x_idxNA[i]
-						d1 <- x_idxNA[i] - table[y[i]]
-						if (d2 < d1) y[i] <- y[i]+1
-						else y[i] <- y[i]
-					}
-				}
-				idx[idxNA] <- y
-			}
-		} else if (how=="interp"){
-			if (any(idxNA)){
-				x_idxNA <- x[idxNA]
-				y <- findInterval(x_idxNA,table)
+                # Not efficient! But what's the alternative look like?
+                for (i in 1:length(y)){
+                    if (y[i] == 0){
+                        y[i] <- 1
+                    } else if (y[i] == N){
+                        d2 <- table[N] - x_idxNA[i]
+                        d1 <- x_idxNA[i] - table[N-1]
+                        if (d2 < d1) y[i] <- N
+                        else y[i] <- N-1
+                    } else {
+                        d2 <- table[y[i]+1] - x_idxNA[i]
+                        d1 <- x_idxNA[i] - table[y[i]]
+                        if (d2 < d1) y[i] <- y[i]+1
+                        else y[i] <- y[i]
+                    }
+                }
+                idx[idxNA] <- y
+            }
+        } else if (how=="interp"){
+            if (any(idxNA)){
+                x_idxNA <- x[idxNA]
+                y <- findInterval(x_idxNA,table)
 
-				# Not efficient! But what's the alternative look like?
-				for (i in 1:length(y)){
-					if (y[i] == 0){
-						next
-					} else if (y[i] == N){
-						# -1 signifies an upper bound error condition
-						if (x_idxNA[i] > table[N]){
-							 y[i] <- -1
-						} else {
-							d2 <- table[N] - table[N-1]
-							d1 <- x_idxNA[i] - table[N-1]
-							y[i] <- y[i] + unclass(d1)/unclass(d2)
-						}
-					} else {
-						d2 <- table[y[i]+1] - table[y[i]]
-						d1 <- x_idxNA[i] - table[y[i]]
-						y[i] <- y[i] + unclass(d1)/unclass(d2)
-					}
-				}
+                # Not efficient! But what's the alternative look like?
+                for (i in 1:length(y)){
+                    if (y[i] == 0){
+                        next
+                    } else if (y[i] == N){
+                        # -1 signifies an upper bound error condition
+                        if (x_idxNA[i] > table[N]){
+                            y[i] <- -1
+                        } else {
+                            d2 <- table[N] - table[N-1]
+                            d1 <- x_idxNA[i] - table[N-1]
+                            y[i] <- y[i] + unclass(d1)/unclass(d2)
+                        }
+                    } else {
+                        d2 <- table[y[i]+1] - table[y[i]]
+                        d1 <- x_idxNA[i] - table[y[i]]
+                        y[i] <- y[i] + unclass(d1)/unclass(d2)
+                    }
+                }
 
-				idx[idxNA] <- y
+                idx[idxNA] <- y
 
-				if (error.how=='NA') {
-					idx[idx==0|idx==-1] <- nomatch
-				} else if (error.how=='drop'){
-					idx <- idx[idx>0]
-				} else if (error.how=='nearest') {
-					idx[idx==-1] <- N
-					idx[idx==0] <- 1
-				} else if (error.how=='stop') {
-                                    idxBad <- idx<1
-                                    if (any(idxBad))
-                                        stop('no match found for ', sum(idxBad), ' dates, e.g.: ',
-                                             paste(x[which(idxBad)[seq(1,min(3,sum(idxBad)))]], collapse=', '))
-				}
-			}
-		}
+                if (error.how=='NA') {
+                    idx[idx==0|idx==-1] <- nomatch
+                } else if (error.how=='drop'){
+                    idx <- idx[idx>0]
+                } else if (error.how=='nearest') {
+                    idx[idx==-1] <- N
+                    idx[idx==0] <- 1
+                } else if (error.how=='stop') {
+                    idxBad <- idx<1
+                    if (any(idxBad))
+                        stop('no match found for ', sum(idxBad), ' dates, e.g.: ',
+                             paste(x[which(idxBad)[seq(1,min(3,sum(idxBad)))]], collapse=', '))
+                }
+            }
+        }
 
         if (!is.null(offset)) {
             if (abs(as.integer(offset) - offset)>0){
